4b4bfba2cf54efd066a9b20f47a0c61d99daafe9
theolucky889 <theolucky889@gmail.com>
2024-09-08 21:52:33 +0800

    Update Variables.py

--- Basic/Variables.py
+++ Basic/Variables.py
@@ -1,3 +1,13 @@
-username = 'LuckyTendy'
+username = "LuckyTendy"
 print(username)
 print(100)
+
+print(type(username))   # print the type of the variable
+
+x = 600
+print(x)
+y = 900
+print(y)
+
+print(id(x))
+print(id(y))
