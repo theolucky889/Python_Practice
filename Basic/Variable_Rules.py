9da02c55a47a46eea4b1ffd73f35ded6b01d2f87
theolucky889 <theolucky889@gmail.com>
2024-09-08 22:00:34 +0800

    Create Variable_Rules.py

---
+++ Basic/Variable_Rules.py
@@ -0,0 +1,32 @@
+import keyword
+
+keyword_list = keyword.kwlist
+print(keyword_list)
+
+print(keyword.iskeyword("try"))
+# Python Variable Rules
+"""
+- Varibale name must start with a letter or the underscore character
+- Variable name cannot start with a number
+- Variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, _ )
+- Variable names are case-sensitive 
+- You cannot use the reserved keywords for naming the variable0    
+"""
+
+_test = "LuckyTendy"
+test = "LuckyTendy"
+print(_test)
+print(test)
+
+Test_80 = "LuckyTendy"
+print(Test_80)
+
+TEST_test = 90
+test_test = 80
+print(TEST_test)
+print(test_test)
+
+returnn = 656565
+print(returnn)
+
+
