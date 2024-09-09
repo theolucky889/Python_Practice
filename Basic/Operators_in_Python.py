7a9506ea2a9978d88625956a77b146f0b6a861ca
theolucky889 <theolucky889@gmail.com>
2024-09-09 21:47:56 +0800

    Update Operators_in_Python.py

--- Basic/Operators_in_Python.py
+++ Basic/Operators_in_Python.py
@@ -11,8 +11,64 @@ print(x ** y)
 print(x // y)
 
 # Assignment Operators
+x = 10
+print(x)
+x = 10 + 2
+print(x)
+x += 2
+print(x)
+x -= 4
+print(x)
+x /= 4
+print(x)
+x *= 4
+print(x)
+x **= 4
+print(x)
+x //= 4
+print(x)
+
 # Comparison Operators
+x = 10
+y = 6
+print(x >= y)
+print(x == y)
+print(x <= y)
+print(x > y)
+print(x < y)
+print(x != y)
+
 # Logical Operators
+x = 10
+y = 4
+print(x == y and x > y)
+print(x == y or x > y)
+
 # Identity Operators
+x = ["LuckyTendy", "LT"]
+y = ["LuckyTendy", "LT"]
+print(x is y)
+print(x is not y)
+
 # Membership Operators
+x = ["LuckyTendy", "LT"]
+y = ["LuckyTendy", "LT"]
+print("LT" in y)
+print("LT" not in y)
+
 # Bitwise Operators
+"""
+& AND
+| OR
+~ NOT
+^ XOR
+"""
+# 0 = 00
+# 1 = 01
+# 2 = 10
+# 3 = 11
+x = 0
+y = 1
+print(x & y)
+print(x | y)
+print(x ^ y)
