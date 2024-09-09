4a2fdb4310997b1d0a01194cc47e529b35a3e80b
theolucky889 <theolucky889@gmail.com>
2024-09-09 21:52:59 +0800

    Update python_arithmetic_operators.py

---
+++ Basic/python_arithmetic_operators.py
@@ -0,0 +1,32 @@
+# Addition + 
+x = 10
+y = 2
+z = x + y
+print(z)
+
+# Subtraction -
+x = 10
+y = 2
+z = x - y
+print(z)
+
+# Multiplication *
+x = 10
+y = 2
+z = x * y
+print(z)
+
+# Modulo (Remainder) %
+x = 10
+y = 2
+z = x % y
+z %= y  # increment
+print(z)
+
+
+# Exponent **
+x = 10
+y = 2
+z = x ** y
+print(z)
+print(**z) # negation
