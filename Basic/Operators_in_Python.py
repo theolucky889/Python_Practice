ffc51cbfd73c0f7bd100807cacc3159083968dae
theolucky889 <theolucky889@gmail.com>
2024-09-09 21:19:58 +0800

    Create Operators_in_Python.py

---
+++ Basic/Operators_in_Python.py
@@ -0,0 +1,18 @@
+# Arithmetic Operators
+x = 15
+y = 4
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+# Assignment Operators
+# Comparison Operators
+# Logical Operators
+# Identity Operators
+# Membership Operators
+# Bitwise Operators
