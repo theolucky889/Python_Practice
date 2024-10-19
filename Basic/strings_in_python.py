96ec8ab98842e4abe0f242f9d09e65144a0eda69
theolucky889 <theolucky889@gmail.com>
2024-10-20 01:20:21 +0800

    Update strings_in_python.py

--- Basic/strings_in_python.py
+++ Basic/strings_in_python.py
@@ -1,5 +1,19 @@
-x = "this is a string"
-y = 'this is a string in single quotes'
+x = "this is a 'string'"
+y = 'this is a \'string\' in single quotes'
 
+z = """
+this is a 
+multiline comment
+"""
+
+c = '''this is a also a
+multiline comment'''
+
+print(x[0])
 print(x)
 print(y)
+print(z)
+print(z[6])
+print(c)
+print("is" in c)
+print("iss" in c)
