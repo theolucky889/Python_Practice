740bb46fd74549c313340562f3198f0b8c26f23d
theolucky889 <theolucky889@gmail.com>
2024-10-20 01:43:16 +0800

    Create string_slicing.py

---
+++ Basic/string_slicing.py
@@ -0,0 +1,8 @@
+# s[i:j] - slice of s from i to j
+# s[i:j:k] - slice of s from i to j with step k
+# s[startindex:endindex:step]
+
+s = "welcome to taiwan with theodore lucky tendy"
+print(s[-1])
+print(s[0:8])
+print(s[2:5:9])
