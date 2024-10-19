60ca7b4a7689f2a8417423af3672da21e17e9d2a
theolucky889 <theolucky889@gmail.com>
2024-10-20 01:27:54 +0800

    Update strings_function_in_python.py

---
+++ Basic/strings_function_in_python.py
@@ -0,0 +1,15 @@
+# len(s): Return the number of items in an object, if object is string it will return the string length
+# str(): Converts a specified value into a String
+# upper(): Converts a string into upper case
+# count(sub[, start[, end]]): Returns the number of times a specified value is found in the string
+# isupper(): returns True if all characters in the string are upper case
+# islower(): Returns True if all character in the string are lower case
+# split(sep=None, maxsplit=-1): Splits the string at the specified seperator, and reutns a list
+# rsplit: Splits a string into a list, starting from the right
+# strip(): Returns a trimmed version of the string
+# lstrip([chars]): Removes any leading characters
+# rstrip([chars]): Removes any trailing characters
+# replace(old, new[, count]): Replaces a specified phrase with another specified phrase
+# find(sub[, start[, end]]): Searches the string for a specified value and returns the position of where it was found
+# index(sub[, start[, end]]): Searches thr string for a speicifed value and returns the position of where it was found
+
